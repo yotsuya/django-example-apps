5e0c46664a2be3cb79ada65f5abbb5ecdeb1af93
Kenzo Yotsuya <kyotsuya@iij-ii.co.jp>
2014-08-19 15:03:36 +0900

    part 2: Customize the admin change list

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -14,5 +14,9 @@ class PollAdmin(admin.ModelAdmin):
                               'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+
 
 admin.site.register(Poll, PollAdmin)
