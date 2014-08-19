ba8d6ccd7b08bc36a9f5f09a89fa357754b5bbc4
Kenzo Yotsuya <kyotsuya@iij-ii.co.jp>
2014-08-19 20:06:42 +0900

    リクエストダンプ画面を作成

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'requestdump.views.index', name='request_dump'),
 )
--- mysite/requestdump/views.py
+++ mysite/requestdump/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    attributes = [
+        (k, getattr(request, k)) for k in dir(request)
+        if isinstance(getattr(request, k), basestring)
+        and not k.startswith('_')
+    ]
+    request_vars = [
+        ('request.attributes', sorted(attributes)),
+        ('request.COOKIES', sorted(request.COOKIES.iteritems())),
+        ('request.GET', sorted(request.GET.iteritems())),
+        ('request.POST', sorted(request.POST.iteritems())),
+        ('request.META', sorted(request.META.iteritems())),
+    ]
+    return render(request, 'requestdump/index.html', {
+        'request_vars': request_vars,
+    })
